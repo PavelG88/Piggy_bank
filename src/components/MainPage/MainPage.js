dce433c5e15dba6e0a0429029a18751466203eab
Olgamay <olga48915042@yandex.ru>
2021-08-05 00:01:12 +0300

    header html,css

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -5,7 +5,19 @@ class MainPage extends Component {
     render() { 
         return (
             <div className="main-page">
-               <div> Piggy Bank - Начало! </div>
+                <div className="header">
+                    <div className="purpose"> СОЗДАТЬ ЦЕЛЬ </div>
+                    <div className="may_purpose"> МОИ ЦЕЛИ </div>
+                </div>
+                <div className="summ">
+                    <div className="summ_text">Накоплено!!!</div>
+                    <div className="summ_itog">
+                        <div className="summ_itog_num">...</div>
+                        <div className="summ_itog_val">рублей!!!</div>
+                    </div>
+                    
+                    <div>картинка</div>
+                </div>                
             </div>
         );
     }
